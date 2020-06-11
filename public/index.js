fbc7c658a075eb4ef357d524a9736a6e86cf3bd2
garrett <martin.garrettl@gmail.com>
2020-06-11 01:21:52 -0700

    add asynchronous updates to db among other things

--- public/index.js
+++ public/index.js
@@ -4,31 +4,138 @@
  * name: Garrett Martin
  * Email: martgarr@oregonstate.edu
  */
+ function getDeckId() {
+   let path = window.location.pathname;
+   var pathParts = path.split('/');
+   if (pathParts[1] === 'decks') {
+     return pathParts[2];
+   } else {
+     return null;
+   }
+ }
+
+function handleNewDeckModal() {
+  let deckdesc = document.getElementById('deckdesc').value;
+  let decktitle = document.getElementById('decktitle').value;
+  let deckauth = document.getElementById('deckauthor').value;
+
+  if (!deckdesc || !decktitle || !deckauth) {
+    alert('Deck title, author and description are required.');
+  } else {
+    var request = new XMLHttpRequest();
+    request.open('POST', "/decks/newdeck");
+
+    var deckObj = {
+      decktitle: decktitle,
+      deckdesc: deckdesc,
+      deckauthor: deckauth
+    };
+
+    var requestBody = JSON.stringify(deckObj);
+    request.setRequestHeader('Content-Type', 'application/json');
+
+    request.addEventListener('load', function (event) {
+      if (event.target.status === 200) {
+        window.location.replace("/decks");
+      } else {
+        alert("Failed to add new deck.");
+      }
+    });
+
+    request.send(requestBody);
+  }
+}
+
+function handleNewFlashcardModal() {
+  //new flash card stuff here
+}
+
+function handleDeleteDeckButton() {
+  let decktitle = document.getElementById('decktitle').innerText;
+  console.log("in handle " + decktitle);
+
+  var request = new XMLHttpRequest();
+  var requestURL = "/decks/delete";
+  request.open('POST', requestURL);
+
+  var delObj = {
+    decktitle: decktitle
+  };
+
+  var requestBody = JSON.stringify(delObj);
+  request.setRequestHeader('Content-Type', 'application/json');
+
+  request.addEventListener('load', function (event) {
+    if (event.target.status === 200) {
+      window.location.replace("/decks");
+    } else {
+      alert("error deleting deck");
+    }
+  });
+
+  request.send(requestBody);
+}
+
+function storeEditInDB() {
+  let deckdesc = document.getElementById('deckdesc').value;
+  let decktitle = document.getElementById('decktitle').innerText;
+
+  if (!deckdesc) {
+    alert("A deck description is required.")
+  } else {
+    var request = new XMLHttpRequest();
+    var requestURL = "/decks/" + getDeckId() + "/edit";
+    request.open('POST', requestURL);
+
+    var editObj = {
+      decktitle: decktitle,
+      deckdesc: deckdesc
+    };
+
+    var requestBody = JSON.stringify(editObj);
+    request.setRequestHeader('Content-Type', 'application/json');
+
+    request.addEventListener('load', function (event) {
+      if (event.target.status === 200) {
+        window.location.replace("/decks");
+      } else {
+        alert("error updating deck");
+      }
+    });
+
+    request.send(requestBody);
+  }
+
+};
+
+function clearRequiredForm() {
+  //clear required data from form
+};
 
 /*
  * UI interactions
  */
 window.addEventListener('DOMContentLoaded', function () {
 
+  let deleteDeckButton = document.getElementById('deckdelete');
+  if (deleteDeckButton) {
+    deleteDeckButton.addEventListener('click', handleDeleteDeckButton);
+  }
 
+  let saveNewDeckButton = document.getElementById('saveNewDeck');
+  if (saveNewDeckButton) {
+    saveNewDeckButton.addEventListener('click', handleNewDeckModal);
+  }
 
   let addFlashCardButton = document.getElementById('addflashcard');
   if (addFlashCardButton) {
     addFlashCardButton.addEventListener('click', addFlashCard)
   }
 
-  // Remember all of the existing twits in an array that we can use for search.
-  /*
-  var twitElemsCollection = document.getElementsByClassName('twit');
-  for (var i = 0; i < twitElemsCollection.length; i++) {
-    allTwits.push(parseTwitElem(twitElemsCollection[i]));
+  let updateDeckButton = document.getElementById('updatebutt');
+  if (updateDeckButton) {
+    console.log("yes it's there");
+    updateDeckButton.addEventListener('click', storeEditInDB);
   }
-
-  var createDeckButton = document.getElementById('create-deck-button');
-  if (createDeckButton) {
-    createDeckButton.addEventListener('click', showCreateDeckModal);
-  }
-
-  */
 
 });
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var bodyParser = require('body-parser');
 
 //mongodb Atlas stuff
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://appaccess:mWf8vhtgPzbAtPxN@cluster0-wu6cw.mongodb.net/cs290final?retryWrites=true&w=majority;"
+const uri = "mongodb+srv://appaccess:mWf8vhtgPzbAtPxN@cluster0-wu6cw.mongodb.net/cs290final?retryWrites=true&w=majority"
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -27,7 +27,9 @@ MongoClient.connect(uri, { useUnifiedTopology: true }).then(client => {
   console.log("== Connected to Database");
 
   app.use(express.static('public'));
+  app.use(bodyParser.json());
 
+  //app.get() functions start here
   app.get("/", function (req, res, next) {
     res.status(200).render('index');
   });
@@ -58,7 +60,6 @@ MongoClient.connect(uri, { useUnifiedTopology: true }).then(client => {
 
   app.get("/decks/:deckId/edit", function (req, res, next) {
     db.collection('decks').find().toArray().then(results => {
-      console.log(results);
       let deckid = req.params.deckId;
       if (results[deckid]) {
         res.status(200).render('edit', results[deckid]);
@@ -75,6 +76,51 @@ MongoClient.connect(uri, { useUnifiedTopology: true }).then(client => {
     res.status(404).render('404');
   });
 
+  //app.post() functions start here
+
+  app.post('/decks/:deckId/edit', function (req, res, next) {
+    if (req.body && req.body.deckdesc && req.body.decktitle) {
+      let deck = db.collection('decks');
+
+      deck.findOneAndUpdate(
+        { decktitle: req.body.decktitle },
+        { $set: { deckdesc: req.body.deckdesc }}
+      ).then(result => {
+        console.log("== deck updated successfully")
+        res.status(200).send("Deck updated successfully");
+      }).catch(error => console.error(error));
+    }
+  });
+
+  app.post('/decks/newdeck', function (req, res, next) {
+    if (req.body && req.body.deckdesc && req.body.decktitle && req.body.deckauthor) {
+      let deck = db.collection('decks');
+
+      let deckObj = {
+        decktitle: req.body.decktitle,
+        deckauthor: req.body.deckauthor,
+        deckdesc: req.body.deckdesc,
+        cards: []
+      };
+
+      deck.insertOne(deckObj).then(result => {
+        console.log("== Deck added successfully.");
+        res.status(200).send("Deck added successfully.");
+      }).catch(error => console.error(error));
+    }
+  });
+
+  app.post('/decks/delete', function(req, res, next) {
+    if (req.body && req.body.decktitle) {
+      let deck = db.collection('decks');
+      let query = { decktitle: req.body.decktitle };
+
+      deck.deleteOne(query).then(result => {
+        console.log("== Deck deleted successfully.");
+        res.status(200).send("Deck deleted successfully.");
+      }).catch(error => console.error(error));
+    }
+  });
 
   app.listen(port, function() {
     console.log("== Server is listening on port ", port);
